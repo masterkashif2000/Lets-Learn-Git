b6bdfd187673624a4572380c5a40c4870c45bd22
Kashif Anwar <masterkashif2000@gmail.com>
2021-09-25 20:47:11 +0530

    added Multiply.java

--- multiply.java
+++ multiply.java
@@ -1,10 +1,8 @@
-import javax.print.event.PrintJobListener;
-
-public class sum{
+public class multiply {
     public static void main(String[] args) {
         int a=10;
         int b=20;
-        int c=a+b;
+        int c=a*b;
         System.out.println("the sum is"+c);
     }
 }
